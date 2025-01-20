57f8cc87683db7cde4a0d17c75dad3fc129dd1bf
John | Elite Encoder <john@eliteencoder.net>
2025-01-20 12:19:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="StreamDiffusion project dependencies",
     install_requires=[
-        "torch>=2.1.0",
+        "torch>=2.3.0",
         "torchvision>=0.16.0",
         "xformers",
         "huggingface-hub>=0.25.0",
